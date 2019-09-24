6e8f0b7a379a1ed3af86cf887e49a3781ace6774
Karim Elgazar <karimelgazar565@gmail.com>
2019-09-24 22:25:58 +0200

    some changes in open jupyter

--- Open_Jupyter.py
+++ Open_Jupyter.py
@@ -1,9 +1,3 @@
-'''  
-Use The Line Below At The Beginning Of The Script
-Then Save It As .bat To Open It In The Original CMD
-
-  @py.exe E:\karim\Py_Automate\Open_Jupyter.py %*
-'''
 import sys
 from subprocess import Popen as pop
 
