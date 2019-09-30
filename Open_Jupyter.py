20ea3fcb2519f2da6523e6fffa7885fbe0d65016
Karim Elgazar <karimelgazar565@gmail.com>
2019-09-30 11:11:13 +0200

    added jupyter dark theme command

--- Open_Jupyter.py
+++ Open_Jupyter.py
@@ -2,7 +2,8 @@ import sys
 from subprocess import Popen as pop
 
 BASE_PATH = sys.path[0]
-
-COMMAND = "pushd %s && jupyter notebook" % BASE_PATH
+ORIGINAL_JUPYTER_THEME = 'jt -r '
+DARK_THEME = 'jt -t onedork -fs 95 -altp -tfs 11 -nfs 115 -cellw 88% -T'
+COMMAND = "pushd %s && %s && jupyter notebook" % (BASE_PATH, DARK_THEME)
 
 pop(COMMAND, shell=True)
