5339c49399295bd9dbf178219426115e0e87253f
Karim Elgazar <karimelgazar565@gmail.com>
2019-10-03 18:25:51 +0200

    magic func doesn't work in scripts

--- Jupyter/00-startup.py
+++ Jupyter/00-startup.py
@@ -9,7 +9,7 @@ jtplot.style(theme='onedork', context='notebook', ticks=True, grid=False)
 InteractiveShell.ast_node_interactivity = "all"
 line = '%'*80
 
-%matplotlib notebook
+# %matplotlib notebook
 
 """
 ********************* VERY IMPORTANT *********************
