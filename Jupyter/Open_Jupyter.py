4d4babb27e0deeec1f14d43eb3c9df0e3e05c409
Karim Elgazar <karimelgazar565@gmail.com>
2020-01-13 16:44:11 +0200

    change the variable name with more descriptive one

--- Jupyter/Open_Jupyter.py
+++ Jupyter/Open_Jupyter.py
@@ -1,9 +1,9 @@
 import sys
 from subprocess import Popen as pop
 
-BASE_PATH = sys.path[0]
+SCRIPT_BASE_PATH = sys.path[0]
 ORIGINAL_JUPYTER_THEME = 'jt -r '
 DARK_THEME = 'jt -t onedork -fs 140 -altp -tfs 150 -dfs 120 -ofs 120 -nfs 115 -cellw 88% -T'
-# COMMAND = "pushd %s && %s && jupyter notebook" % (BASE_PATH, DARK_THEME)
-COMMAND = "pushd %s && jupyter notebook" % BASE_PATH
+# COMMAND = "pushd %s && %s && jupyter notebook" % (SCRIPT_BASE_PATH, DARK_THEME)
+COMMAND = "pushd %s && jupyter notebook" % SCRIPT_BASE_PATH
 pop(COMMAND, shell=True)
