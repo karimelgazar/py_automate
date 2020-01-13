3f521bb5336b1f2245488904d798b7c29312177f
Karim Elgazar <karimelgazar565@gmail.com>
2020-01-13 18:05:22 +0200

    add emoji

--- Udacimak Decoration/decorate.py
+++ Udacimak Decoration/decorate.py
@@ -4,46 +4,38 @@ from tkinter import filedialog, Tk
 import os
 
 
-#TODO ADD emoji to the buttons
+# TODO ADD a shortcut to collapse the side bar (Alt+b)
 
-#TODO ADD a shortcut to collapse the side bar (Alt+b)
+# TODO ADD a button to move to the next lesson in the last concept
 
-#TODO ADD a button to move to the next lesson in the last concept
-
-#TODO ADD a button to move to the previous lesson in the first concept
+# TODO ADD a button to move to the previous lesson in the first concept
 
 def decorate(html_file, files):
     """
-    THIS METHOD: adds decoration to the html files 
+    THIS METHOD: adds decoration to the html files
     you sholud edit the decoration.html file
-    to add a new decoration to the html 
+    to add a new decoration to the html
 
     Arguments:
     =========
         html_file  -- the html file to edit
         files  -- all html files in this folder
     """
-    
+
     decoration = open('decoration.html', 'r').read()
 
-    previous_button = ''
-    next_button = ''
+    previous = ''
+    next = ''
     indx = files.index(html_file)
 
-    # scrollbar = "\t\tconst currentInSideBar = $(\"ul.sidebar-list.components li a:contains(\'{}\')\")".format(
-    #     html_file[:-5])
 
-    # mCSB_1_container > ul.sidebar-list.list-unstyled.components > li:nth-child(32) > a
-    scrollbar = "\t\tconst currentInSideBar = $(\"#mCSB_1_container > ul.sidebar-list.list-unstyled.components > li:nth-child({}) > a\")".format(
-        indx + 1)
+   scrollbar_index = indx + 1
 
-    if indx > 0:
-        previous_button = "<a href=\"{}\" class=\"btn btn-warning\" role=\"button\" style=\"font-size : 50px; width: 100%; height: 75%px;\">Previous Concept</a>".format(
-            files[indx - 1])
+   if indx > 0:
+        previous = files[indx - 1]
 
     if indx < (len(files) - 1):
-        next_button = "<a href=\"{}\" class=\"btn btn-success\" role=\"button\" style=\"font-size : 50px; width: 100%; height: 75%px;\">Next Concept</a>".format(
-            files[indx + 1])
+        next = files[indx + 1]
 
     # if ascii errors occured when
     #  encoding to UTF-8 just ignore them
@@ -51,7 +43,6 @@ def decorate(html_file, files):
 
     with open(html_file, "w", encoding="UTF-8", errors='ignore') as new:
         for line in old:
-
             # this is so important you should use
             # </main> so that you can add new features freely
             # and not to affect the page original source code
@@ -59,10 +50,10 @@ def decorate(html_file, files):
                 new.write(line)  # write this line
 
                 # add the decoration
-                new_decoration = decoration.replace('XX1', next_button)
-                new_decoration = new_decoration.replace('XX2', previous_button)
+                new_decoration = decoration.replace('XX1', next)
+                new_decoration = new_decoration.replace('XX2', previous)
 
-                new.write(new_decoration.replace('XX3', scrollbar))
+                new.write(new_decoration.replace('XX3', scrollbar_index))
                 print("Done With File: {}".format(html_file))
                 return
             else:
