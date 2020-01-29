0221cb4c65ff4b96cdf4eb24019ce5ae4c0a74b2
Karim Elgazar <karimelgazar565@gmail.com>
2020-01-29 08:19:29 +0200

    initial commit

---
+++ Euler Thumbnail/create_euler_thumbnail.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageFont, ImageDraw
+import numpy as np
+import cv2
+import webbrowser
+
+
+problem_number = 'k'
+while not problem_number.isdigit():
+    problem_number = input("please input a vaild the probelm number: ".title())
+
+problem_number = '#' + problem_number.zfill(3)
+
+coordination = (600, 650)
+color = (67, 195, 244)  # BGR
+size = 300  # font size
+
+thumbnail = cv2.imread("Euler Thumbnail/euler.png")
+
+#
+# Make into PIL Image
+im_p = Image.fromarray(thumbnail)
+
+# Get a drawing context
+draw = ImageDraw.Draw(im_p)
+nirmala_bold = ImageFont.truetype(
+    "C:/Windows/Fonts/Nirmala UI/NirmalaB.ttf", size)
+draw.text(coordination,
+          problem_number,
+          color,
+          font=nirmala_bold)
+
+# Convert back to OpenCV image and save
+result_o = np.array(im_p)
+
+# Save the Thumbnail
+output_path = 'E:/Captures/'
+cv2.imwrite(output_path + problem_number + '.png', result_o)
+webbrowser.open(output_path)
