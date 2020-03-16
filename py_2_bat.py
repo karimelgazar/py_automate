0be8d7e48b2f4b92bfe521e8c201de370cd30edd
Karim Elgazar <karimelgazar565@gmail.com>
2020-03-16 22:49:12 +0200

    remove original python script batch does not need to the orignal python script to work

--- py_2_bat.py
+++ py_2_bat.py
@@ -8,18 +8,15 @@ files = filedialog.askopenfilenames()
 
 for file in files:
     print("THE FILE:", file)
-    with open(file, 'r', encoding="UTF-8") as py:
-        py_name = os.path.basename(file)
+    py_name = os.path.basename(file)
 
-        if py_name == '00-startup.py':
-            continue
+    if py_name == '00-startup.py':
+        continue
 
-        bat_name = "".join([i[0] for i in py_name.split('_')])
-        bat_name = "{}\Batches\{}.bat".format(BASE_DIR, bat_name)
+    bat_name = "".join([i[0] for i in py_name.split('_')])
+    bat_name = "{}\Batches\{}.bat".format(BASE_DIR, bat_name)
 
-        with open(bat_name, 'w', encoding="UTF-8") as bat:
-            bat.write("@python \"{}\"\n".format(file))
-            bat.write("IF ERRORLEVEL 1 PAUSE && EXIT\n\n")
-            bat.write(py.read())
+    with open(bat_name, 'w', encoding="UTF-8") as bat:
+        bat.write("@python \"{}\" %*\n".format(file))
 
-        print(bat_name)
+    print(bat_name)
