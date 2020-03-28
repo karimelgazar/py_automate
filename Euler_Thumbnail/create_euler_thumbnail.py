10174a7ef83ccaa0cde96a145787bd6a8820bc90
Karim Elgazar <karimelgazar565@gmail.com>
2020-03-28 23:38:38 +0200

    add description and keywords to clipboard

--- Euler_Thumbnail/create_euler_thumbnail.py
+++ Euler_Thumbnail/create_euler_thumbnail.py
@@ -14,12 +14,46 @@ parser.add_argument('number', help='the problem number'.title(), type=int)
 problem_number = str(parser.parse_args().number)
 problem_number = '#' + problem_number.zfill(3)
 
-video_name = "مشروع أويلر مسألة Project Euler Problem {} || {} ".format(
+video_description = '''مشروع أويلر مسألة Project Euler Problem {} | | {} 
+السلام عليكم يا شباب فى الفيديو ده ان شاء الله هنحل مسالة رقم {} من مشروع أويلر
+
+يا رب الفيديو يعجبكم وماتنساش تعمل اشتراك  🔥🔥 
+ و لايك 👍👍  و تفعل الجرس 🔔🔔
+ عشان يوصلك كل جديد وشير الفيديو عشان المنفعة تعم 
+=====================================================
+لو مش عارف يعنى ايه Jupyter او ازاى تستخدمه شوف الفيديو ده
+===============================
+https://www.youtube.com/watch?v=1Hn4wc_M3Ew
+=====================================================
+كل المسائل المحلولة
+=================
+https://www.youtube.com/playlist?list=PLO1D3YWS7ep3Zrh8B4SrhIsyxneg23x29
+=====================================================
+الحلول على Github
+=================
+https://github.com/karimelgazar/Project-Euler
+=====================================================
+Please Support Me On:
+Linkedin:
+https://www.linkedin.com/in/karim-elgazar
+Facebook:
+https://www.facebook.com/karimCodes
+Twitter:
+https://www.twitter.com/karimCodes
+Github:
+https://github.com/karimelgazar
+===================================================
+#karimCodes,arabic,عربي,introduction,Python (Programming Language),Tutorial (Media Genre),متطابقة أويلر,متطابقة اويلر,math pi hanan,mathpihanan,اويلر,وش سر جمال متطابقة اويلر,project euler,مشروع اويلر,حل مسألة,problem,project euler problem,أويلر,رياضيات,حساب,maths
+===================================================
+#karimCodes #LearnWithKarim #KarimElgazar'''.format(
     problem_number,
-    problem_number)
+    problem_number,
+    # ! don't include the hashtag sign (#) for the description part
+    problem_number[1:]
+)
 
 
-pyperclip.copy(video_name)
+pyperclip.copy(video_description)
 
 coordination = (600, 650)
 color = (67, 195, 244)  # BGR
